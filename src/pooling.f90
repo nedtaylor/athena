20ac6af027bd9572f3d0558a174e3530f5858e1f
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-07-07 17:02:00 +0100

    Tidy up

--- src/pooling.f90
+++ src/pooling.f90
@@ -1,12 +1,15 @@
-!!!###################################################################################
-!!!###################################################################################
-!!!###################################################################################
+!!!#############################################################################
+!!! Code written by Ned Thaddeus Taylor
+!!! Code part of the ARTEMIS group (Hepplestone research group)
+!!! Think Hepplestone, think HRG
+!!!#############################################################################
 module PoolingLayer
   use constants, only: real12
   implicit none
 
-  integer :: pool_size     ! Pooling window size (assumed square)
-  integer :: pool_stride   ! Pooling stride
+  integer :: pool_size     ! pooling window size (assumed square)
+  integer :: pool_stride   ! pooling stride
+
 
   private
 
@@ -14,6 +17,9 @@ module PoolingLayer
 
 contains
 
+!!!#############################################################################
+!!! 
+!!!#############################################################################
   subroutine initialise(size, stride)
     implicit none
     integer :: size, stride
@@ -28,12 +34,12 @@ contains
 
     integer :: i, j, k, l, m, n
     
-    ! Compute the size of the input and output feature maps
+    !! compute the size of the input and output feature maps
     integer :: input_size, output_size
     input_size = size(input, 1)
     output_size = (input_size - pool_size) / pool_stride + 1
 
-    ! Perform the pooling operation
+    !! perform the pooling operation
     do m = 1, size(input, 3)
        do l = 1, size(input, 2), pool_stride
           do j = 1, output_size
@@ -46,47 +52,48 @@ contains
        end do
     end do
   end subroutine forward
+!!!#############################################################################
 
+
+!!!#############################################################################
+!!! 
+!!!#############################################################################
   subroutine backward(input, output_gradients, input_gradients)
     implicit none
     real(real12), dimension(:,:,:), intent(in) :: input
     real(real12), dimension(:,:,:), intent(in) :: output_gradients
     real(real12), dimension(:,:,:), intent(out) :: input_gradients
 
-    integer :: i, j, k, l, m, n
+    integer :: i, j, k, m, n, istride, jstride
     integer :: input_size, output_size
     integer, dimension(2) :: max_index
 
-    ! Initialise input_gradients to zero
+    !! initialise input_gradients to zero
     input_gradients = 0._real12
 
-    ! Compute the size of the input and output feature maps
+    !! compute the size of the input and output feature maps
     input_size = size(input, 1)
     output_size = (input_size - pool_size) / pool_stride + 1
-
-    ! Compute gradients for input feature map
+    
+    !! compute gradients for input feature map
     do m = 1, size(input, 3)
-       do l = 1, size(input, 2), pool_stride
-          do j = 1, output_size
-             do i = 1, output_size
-                ! Find the index of the maximum value in the corresponding pooling window
-                max_index = maxloc(input((i-1)*pool_stride+1:(i-1)*pool_stride+pool_size, &
-                     (j-1)*pool_stride+1:(j-1)*pool_stride+pool_size, m))
+       do j = 1, output_size
+          jstride = (j-1)*pool_stride
+          do i = 1, output_size
+             istride = (i-1)*pool_stride
+             !! find the index of the maximum value in the corresponding pooling window
+             max_index = maxloc(input(istride+1:istride+pool_size, &
+                  jstride+1:jstride+pool_size, m))
 
-                ! Compute gradients for input feature map
-                input_gradients((i-1)*pool_stride+max_index(1), &
-                     (j-1)*pool_stride+max_index(2), m) = output_gradients(i, j, m)
-                
-                !input_gradients((i-1)*pool_stride+1:(i-1)*pool_stride+pool_size, &
-                !     (j-1)*pool_stride+1:(j-1)*pool_stride+pool_size, m)) = output_gradients(i,j,m)
-             end do
+             !! compute gradients for input feature map
+             input_gradients(istride+max_index(1), &
+                  jstride+max_index(2), m) = output_gradients(i, j, m)
+
           end do
        end do
     end do
   end subroutine backward
+!!!#############################################################################
 
 end module PoolingLayer
-
-!!!###################################################################################
-!!!###################################################################################
 !!!###################################################################################
