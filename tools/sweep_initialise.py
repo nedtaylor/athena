eff691e577f365bb39358cdbf2eac9143bb83b61
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-07-18 09:25:00 +0100

    Convert batch_size and l2_lambda to list

--- tools/sweep_initialise.py
+++ tools/sweep_initialise.py
@@ -10,22 +10,22 @@ sweep_configuration = {
     },
     'parameters':
     {
-        'num_epochs': {'max': 20, 'min': 10},
-        'batch_size': {'max': 40, 'min': 10},
+        'num_epochs': {'max': 10, 'min': 5},
+        'batch_size': {'values': [16, 32, 64]},
         'learning_rate': {'max': 0.1, 'min': 0.005},
-        #'momentum': {'max': 0.9, 'min': 0.0},
         'l1_lambda': {'max': 0.75, 'min': 0.0},
-        'l2_lambda': {'max': 0.75, 'min': 0.0},
+        'l2_lambda': {'values': [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0]},
         'cv_num_filters': {'max': 32, 'min': 2},
         'cv_kernel_size': {'max': 5, 'min': 3},
         'cv_stride': {'max': 2, 'min': 1},
         'pool_kernel_size': {'max': 4, 'min': 2},
         'pool_stride': {'max': 4, 'min': 2},
-        'cv_clip_norm': {'max': 50.0, 'min': 1.0},
-        'fc_clip_norm': {'max': 50.0, 'min': 1.0},
-        'num_hidden_layers': {'max': 3, 'min': 1},
-        'beta1': {'max':0.9, 'min': 0.5},
-        'beta2': {'max':0.999, 'min': 0.7}
+        'cv_clip_norm': {'max': 100.0, 'min': 1.0},
+        'fc_clip_norm': {'max': 100.0, 'min': 1.0},
+        'num_hidden_layers': {'max': 2, 'min': 1},
+        #'beta1': {'max': 0.9, 'min': 0.5},
+        #'beta2': {'max': 0.999, 'min': 0.7},
+        'momentum': {'max': 0.9, 'min': 0.0}
     }
 }
 
