37f33899d490ab496c412ce1e2014d2cb62a5f04
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-08-02 10:28:55 +0100

    Handle flatten layers

--- src/lib/mod_container_layer_sub.f90
+++ src/lib/mod_container_layer_sub.f90
@@ -7,6 +7,8 @@ submodule(container_layer) container_layer_submodule
   use conv2d_layer, only: conv2d_layer_type
   use maxpool2d_layer, only: maxpool2d_layer_type
   use full_layer, only: full_layer_type
+  use flatten2d_layer, only: flatten2d_layer_type
+  use flatten3d_layer, only: flatten3d_layer_type
 
 contains
   
@@ -21,6 +23,10 @@ contains
     type is(maxpool2d_layer_type)
        call this%layer%forward(previous%output)
     type is(full_layer_type)
+       call this%layer%forward(previous%output)
+    type is(flatten2d_layer_type)
+       call this%layer%forward(previous%output)
+    type is(flatten3d_layer_type)
        call this%layer%forward(previous%output)
     end select
 
@@ -39,6 +45,10 @@ contains
     type is(maxpool2d_layer_type)
        call this%layer%backward(previous%output, gradient)
     type is(full_layer_type)
+       call this%layer%backward(previous%output, gradient)
+    type is(flatten2d_layer_type)
+       call this%layer%backward(previous%output, gradient)
+    type is(flatten3d_layer_type)
        call this%layer%backward(previous%output, gradient)
     end select
 
