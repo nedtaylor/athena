c93c5ebc0e8555bc6b0ff413dd69bbef797ca2ba
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-08-10 20:27:12 +0100

    Fix half kernel definition

--- tmp12.f90
+++ tmp12.f90
@@ -1,37 +1,32 @@
 program main
   implicit none
 
-  integer :: input_x=7, input_y=5, output_x=3, output_y=2
-  integer :: stride_x = 3, stride_y = 2
+  integer :: input_x=5, input_y=5, output_x=2, output_y=2
+  integer :: stride_x = 2, stride_y = 2
   integer :: kernel_x = 3, kernel_y = 3
   integer :: num_channels = 1, num_filters = 1
   integer :: pad_x = 0, pad_y = 0
   
   integer :: i,j,l,m, a, b
-  integer :: half_x, half_y, centre_x, centre_y
+  integer :: half_x, half_y
   integer :: iend_idx, jend_idx
   integer :: i_start,j_start,i_end,j_end,x_start,y_start,x_end,y_end
   integer :: ioffset, joffset, istride, jstride
-  integer :: k_x, i_x, k_y, i_y, int1, int2
+  integer :: k_x, i_x, k_y, i_y, int_x, int2, n_stride_x
   
-  half_x = kernel_x/2
-  half_y = kernel_y/2
-  centre_x = 2 - mod(kernel_x,2)
-  centre_y = 2 - mod(kernel_y,2)
-  iend_idx = half_x + (centre_x - 1)
-  jend_idx = half_y + (centre_y - 1)
+  half_x = (kernel_x-1)/2
+  half_y = (kernel_y-1)/2
+  iend_idx = half_x + (1 - mod(kernel_x,2))
+  jend_idx = half_y + (1 - mod(kernel_y,2))
   ioffset  = 1 + half_x - pad_x
   joffset  = 1 + half_y - pad_y
 
-  write(*,*) "LOOK HERE", mod(-5,3)
-
   k_x = kernel_x - 1
   i_x = input_x + stride_x
-  int1 = kernel_x - input_x + (output_x -1)*stride_x
+  !int_x = kernel_x - input_x + (output_x -1)*stride_x + (2 - kernel_x)
+  int_x = (output_x -1)*stride_x + 1 + iend_idx
+  n_stride_x = output_x * stride_x
   
-  write(*,*) "LOOK NED", int1
-  !int1 = mod(input_x-1,2)
-  !int2 = mod(input_x,2)
 
   
   !! do the 4 corners separately? This is kernel size dependent
@@ -50,18 +45,14 @@ program main
      !!   ... 2. current lowest output overlapping with left of kernel (this is a repeating pattern until 1. takes over as min)
      !!   ...)
 !!! should it only be kernel_x - 1 - i if it fits perfectly?
-!!! otherwise, it should be kernel_x - 1 + int1
-     x_start = max(k_x-i,        -half_x + mod(input_x*stride_x-i + stride_x-kernel_x,stride_x))
+!!! otherwise, it should be kernel_x - 1 + int_x
+     x_start = max(k_x-i,    -half_x  + mod(n_stride_x - i + kernel_x,stride_x))
      !! min( ...
      !!   ... 1. distance from final output to centre of scanning kernel
      !!   ... 2. current highest output overlapping with right of kernel (this is a repeating pattern until 1. takes over as min)
      !!   ...)
-     x_end   = min(input_x-i-k_x+1+int1, iend_idx - mod(stride_x+i-1,stride_x))
-     !x_start = max(k_x-i,        -half_x + mod(abs(i_x-(i+1)),stride_x))
-     !x_end   = min(input_x-i-k_x, iend_idx - mod(abs(i_x+(i+1)),stride_x))
-     !!x_start = max(1-i,     -half_x + mod(i-1,stride_x))
-     !!x_end   = min(input_x - i-1, iend_idx - mod(i-1,stride_x))
-     !if(x_start.gt.x_end) cycle
+     x_end   = min(int_x-i,  iend_idx - mod(n_stride_x + i - 1,stride_x))
+     if(x_start.gt.x_end) cycle
 
 
      jstride = (j-joffset)/stride_y + 1
@@ -73,8 +64,8 @@ program main
 
      !! apply full convolution to compute input gradients
      write(*,*) i, j
-     write(*,*) k_x-i,        -half_x + mod(input_x*stride_x-i+ stride_x-kernel_x,stride_x), "max"
-     write(*,*) input_x-i-k_x+1+int1, iend_idx - mod(stride_x+i-1,stride_x), "min",iend_idx
+     write(*,*) k_x-i,    -half_x  + mod(n_stride_x - i + kernel_x,stride_x), "max"
+     write(*,*) int_x-i,  iend_idx - mod(n_stride_x + i - 1,stride_x), "min",iend_idx
      write(*,*) x_end,x_start,-stride_x, "test", (/ (a, a=x_end,x_start,-stride_x) /)
      !write(*,*) y_end,y_start,-stride_y
      write(*,*) 
