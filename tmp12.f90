61616637db01c6fd928e52dd1b083d53acd1cbf4
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-08-10 20:43:56 +0100

    Implement y

--- tmp12.f90
+++ tmp12.f90
@@ -1,9 +1,9 @@
 program main
   implicit none
 
-  integer :: input_x=6, input_y=5, output_x=3, output_y=2
+  integer :: input_x=5, input_y=5, output_x=2, output_y=2
   integer :: stride_x = 2, stride_y = 2
-  integer :: kernel_x = 4, kernel_y = 3
+  integer :: kernel_x = 3, kernel_y = 3
   integer :: num_channels = 1, num_filters = 1
   integer :: pad_x = 0, pad_y = 0
   
@@ -12,7 +12,7 @@ program main
   integer :: iend_idx, jend_idx
   integer :: i_start,j_start,i_end,j_end,x_start,y_start,x_end,y_end
   integer :: ioffset, joffset, istride, jstride
-  integer :: k_x, i_x, k_y, i_y, int_x, int2, n_stride_x
+  integer :: k_x, k_y, int_x, int_y, n_stride_x, n_stride_y
   
   half_x = (kernel_x-1)/2
   half_y = (kernel_y-1)/2
@@ -22,17 +22,19 @@ program main
   joffset  = 1 + half_y - pad_y
 
   k_x = kernel_x - 1
-  i_x = input_x + stride_x
+  k_y = kernel_x - 1
   !int_x = kernel_x - input_x + (output_x -1)*stride_x + (2 - kernel_x)
   int_x = (output_x -1)*stride_x + 1 + iend_idx
+  int_y = (output_y -1)*stride_y + 1 + jend_idx
   n_stride_x = output_x * stride_x
+  n_stride_y = output_y * stride_y
   
 
   
   !! do the 4 corners separately? This is kernel size dependent
   do concurrent( &
        i=1:input_x, &
-       j=1:1,&!input_y, &
+       j=1:input_y, &
        m=1:num_channels, &
        l=1:num_filters &
        )
@@ -44,30 +46,33 @@ program main
      !!   ... 1. distance from first output to centre of scanning kernel
      !!   ... 2. current lowest output overlapping with left of kernel (this is a repeating pattern until 1. takes over as min)
      !!   ...)
-!!! should it only be kernel_x - 1 - i if it fits perfectly?
-!!! otherwise, it should be kernel_x - 1 + int_x
      x_start = max(k_x-i,    -half_x  + mod(n_stride_x - i + kernel_x,stride_x))
      !! min( ...
      !!   ... 1. distance from final output to centre of scanning kernel
-     !!   ... 2. current highest output overlapping with right of kernel (this is a repeating pattern until 1. takes over as min)
+     !!   ... 2. current highest output overlapping with right of kernel (this is a repeating pattern until 1. takes over as max)
      !!   ...)
      x_end   = min(int_x-i,  iend_idx - mod(n_stride_x + i - 1,stride_x))
      if(x_start.gt.x_end) cycle
 
 
      jstride = (j-joffset)/stride_y + 1
-     j_start = max(1,            jstride - half_y)
-     j_end   = min(output_y,     jstride + half_y)
-     y_start = max(1 - j,       -half_y) !check old vs new, was I use a rotated version?
-     y_end   = min(output_y - j, jend_idx)    !check old vs new, was I use a rotated version?
-     !if(y_start.gt.y_end) cycle
+     i_start = max(1,             jstride)
+     i_end   = min(output_y,      jstride + (j-1)/stride_y )
+     !! max( ...
+     !!   ... 1. distance from first output to centre of scanning kernel
+     !!   ... 2. current lowest output overlapping with left of kernel (this is a repeating pattern until 1. takes over as min)
+     !!   ...)
+     y_start = max(k_y-j,    -half_y  + mod(n_stride_y - j + kernel_y,stride_y))
+     !! min( ...
+     !!   ... 1. distance from final output to centre of scanning kernel
+     !!   ... 2. current highest output overlapping with right of kernel (this is a repeating pattern until 1. takes over as max)
+     !!   ...)
+     y_end   = min(int_y-j,  jend_idx - mod(n_stride_y + j - 1,stride_y))
+     if(y_start.gt.y_end) cycle
 
      !! apply full convolution to compute input gradients
-     write(*,*) i, j
-     write(*,*) k_x-i,    -half_x  + mod(n_stride_x - i + kernel_x,stride_x), "max"
-     write(*,*) int_x-i,  iend_idx - mod(n_stride_x + i - 1,stride_x), "min",iend_idx
-     write(*,*) x_end,x_start,-stride_x, "test", (/ (a, a=x_end,x_start,-stride_x) /)
-     !write(*,*) y_end,y_start,-stride_y
+     write(*,'(I0,2X,I0)') i, j
+     write(*,*) (/ (a, a=x_end,x_start,-stride_x) /)
      write(*,*) 
      !this%di(i,j,m) = &
      !     this%di(i,j,m) + &
