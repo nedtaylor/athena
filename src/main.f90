7a4fdeaa3a7c5d3bcc86fd5d6d85a33ca341991f
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-08-20 08:12:56 +0100

    Add 4D layer

--- src/main.f90
+++ src/main.f90
@@ -17,6 +17,7 @@ program ConvolutionalNeuralNetwork
 
   use container_layer, only: container_layer_type
   use input3d_layer,   only: input3d_layer_type
+  use input4d_layer,   only: input4d_layer_type
   use full_layer,      only: full_layer_type
   use conv2d_layer,    only: conv2d_layer_type
   use conv3d_layer,    only: conv3d_layer_type
@@ -140,21 +141,54 @@ program ConvolutionalNeuralNetwork
   !! Initialise the convolution layer
   network%num_layers = 6
   allocate(network%model(network%num_layers))
-  allocate(network%model(1)%layer, source = input3d_layer_type(&
-       input_shape = [size(input_images,1),size(input_images,2),input_channels]))
-  allocate(network%model(2)%layer, source = conv2d_layer_type( &
-       input_shape = [28,28,input_channels], &
+  
+  !!allocate(network%model(1)%layer, source = input3d_layer_type(&
+  !!     input_shape = [size(input_images,1),size(input_images,2),input_channels]))
+  !!allocate(network%model(2)%layer, source = conv2d_layer_type( &
+  !!     input_shape = [size(input_images,1),size(input_images,2),input_channels], &
+  !!     num_filters = cv_num_filters, kernel_size = 3, stride = 1, &
+  !!     padding=padding_method, &
+  !!     calc_input_gradients = .false., &
+  !!     activation_function = "relu", clip_dict = cv_clip))
+  !!allocate(network%model(3)%layer, source = maxpool2d_layer_type(&
+  !!     input_shape=[28,28,cv_num_filters], &
+  !!     pool_size=2, stride=2))
+  !!allocate(network%model(4)%layer, source = flatten2d_layer_type(&
+  !!     input_shape=[14,14,cv_num_filters]))
+  !!allocate(network%model(5)%layer, source = full_layer_type( &
+  !!     num_inputs=product([14,14,cv_num_filters]), &
+  !!     num_outputs=100, clip_dict = fc_clip, &
+  !!     activation_function = "relu", &
+  !!     kernel_initialiser="he_uniform", &
+  !!     bias_initialiser="he_uniform" &
+  !!     ))
+  !!allocate(network%model(6)%layer, source = full_layer_type( &
+  !!     num_inputs=100, &
+  !!     num_outputs=10, clip_dict = fc_clip,&
+  !!     activation_function="softmax", &
+  !!     kernel_initialiser="glorot_uniform", &
+  !!     bias_initialiser="glorot_uniform" &
+  !!     ))
+  
+  write(*,*) "HERE", [size(input_images,1),size(input_images,2),1,input_channels]
+  write(*,*) "TEMPORARY! I have had to set dimension 3 width to 3, &
+   &to account for padding that I haven't set up for that dimension for the kernel width"
+
+  allocate(network%model(1)%layer, source = input4d_layer_type(&
+       input_shape = [size(input_images,1),size(input_images,2),3,input_channels]))
+  allocate(network%model(2)%layer, source = conv3d_layer_type( &
+       input_shape = [size(input_images,1),size(input_images,2),3,input_channels], &
        num_filters = cv_num_filters, kernel_size = 3, stride = 1, &
        padding=padding_method, &
        calc_input_gradients = .false., &
        activation_function = "relu", clip_dict = cv_clip))
-  allocate(network%model(3)%layer, source = maxpool2d_layer_type(&
-       input_shape=[28,28,cv_num_filters], &
+  allocate(network%model(3)%layer, source = maxpool3d_layer_type(&
+       input_shape=[28,28,1,cv_num_filters], &
        pool_size=2, stride=2))
-  allocate(network%model(4)%layer, source = flatten2d_layer_type(&
-       input_shape=[14,14,cv_num_filters]))
+  allocate(network%model(4)%layer, source = flatten3d_layer_type(&
+       input_shape=[14,14,1,cv_num_filters]))
   allocate(network%model(5)%layer, source = full_layer_type( &
-       num_inputs=product([14,14,cv_num_filters]), &
+       num_inputs=product([14,14,1,cv_num_filters]), &
        num_outputs=100, clip_dict = fc_clip, &
        activation_function = "relu", &
        kernel_initialiser="he_uniform", &
@@ -261,7 +295,9 @@ program ConvolutionalNeuralNetwork
   end do
 
   write(6,*) "Starting training..."
-  call network%train(input_images, input_labels, num_epochs, batch_size, &
+  !!call network%train(input_images, input_labels, num_epochs, batch_size, &
+  !!     plateau_threshold, shuffle_dataset, 20, verbosity)
+  call network%train(spread(input_images,3,3), input_labels, num_epochs, batch_size, &
        plateau_threshold, shuffle_dataset, 20, verbosity)
   write(*,*) "Training finished"
 
