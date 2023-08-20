9239791c5e085185111e87b0e89487db0350ee6e
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-08-20 08:12:05 +0100

    Add 3D and 4D layer handling

--- src/lib/mod_container_layer_sub.f90
+++ src/lib/mod_container_layer_sub.f90
@@ -4,12 +4,15 @@
 !!! Think Hepplestone, think HRG
 !!!#############################################################################
 submodule(container_layer) container_layer_submodule
-  use conv2d_layer, only: conv2d_layer_type
-  use maxpool2d_layer, only: maxpool2d_layer_type
   use input3d_layer, only: input3d_layer_type
-  use full_layer, only: full_layer_type
+  use input4d_layer, only: input4d_layer_type
+  use conv2d_layer, only: conv2d_layer_type
+  use conv3d_layer, only: conv3d_layer_type
+  use maxpool2d_layer, only: maxpool2d_layer_type
+  use maxpool3d_layer, only: maxpool3d_layer_type
   use flatten2d_layer, only: flatten2d_layer_type
   use flatten3d_layer, only: flatten3d_layer_type
+  use full_layer, only: full_layer_type
 
 contains
   
@@ -21,15 +24,25 @@ contains
     select type(previous => input%layer)
     type is(input3d_layer_type)
        call this%layer%forward(previous%output)
+    type is(input4d_layer_type)
+       call this%layer%forward(previous%output)
+
     type is(conv2d_layer_type)
        call this%layer%forward(previous%output)
+    type is(conv3d_layer_type)
+       call this%layer%forward(previous%output)
+
     type is(maxpool2d_layer_type)
        call this%layer%forward(previous%output)
-    type is(full_layer_type)
+    type is(maxpool3d_layer_type)
        call this%layer%forward(previous%output)
+
     type is(flatten2d_layer_type)
        call this%layer%forward(previous%output)
     type is(flatten3d_layer_type)
+       call this%layer%forward(previous%output)
+
+    type is(full_layer_type)
        call this%layer%forward(previous%output)
     end select
 
@@ -45,15 +58,25 @@ contains
     select type(previous => input%layer)
     type is(input3d_layer_type)
        call this%layer%backward(previous%output, gradient)
+       type is(input4d_layer_type)
+          call this%layer%backward(previous%output, gradient)
+
     type is(conv2d_layer_type)
        call this%layer%backward(previous%output, gradient)
+    type is(conv3d_layer_type)
+       call this%layer%backward(previous%output, gradient)
+
     type is(maxpool2d_layer_type)
        call this%layer%backward(previous%output, gradient)
-    type is(full_layer_type)
+    type is(maxpool3d_layer_type)
        call this%layer%backward(previous%output, gradient)
+
     type is(flatten2d_layer_type)
        call this%layer%backward(previous%output, gradient)
     type is(flatten3d_layer_type)
+       call this%layer%backward(previous%output, gradient)
+
+    type is(full_layer_type)
        call this%layer%backward(previous%output, gradient)
     end select
 
