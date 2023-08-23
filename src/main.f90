88de277d8f7f15be00581cf8d78723170fe99662
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-08-23 10:08:49 +0100

    Remove layer-dependent clip defining

--- src/main.f90
+++ src/main.f90
@@ -150,17 +150,17 @@ program ConvolutionalNeuralNetwork
            num_filters = cv_num_filters, kernel_size = 3, stride = 1, &
            padding=padding_method, &
            calc_input_gradients = .false., &
-           activation_function = "relu", clip_dict = cv_clip))
+           activation_function = "relu"))
      call network%add(maxpool2d_layer_type(&
            pool_size = 2, stride = 2))
      call network%add(full_layer_type( &
-           num_outputs = 100, clip_dict = fc_clip, &
+           num_outputs = 100, &
            activation_function = "relu", &
            kernel_initialiser = "he_uniform", &
            bias_initialiser = "he_uniform" &
            ))
      call network%add(full_layer_type( &
-           num_outputs = 10, clip_dict = fc_clip,&
+           num_outputs = 10,&
            activation_function = "softmax", &
            kernel_initialiser = "glorot_uniform", &
            bias_initialiser = "glorot_uniform" &
@@ -171,17 +171,17 @@ program ConvolutionalNeuralNetwork
      !!     num_filters = cv_num_filters, kernel_size = [3,3,1], stride = 1, &
      !!     padding = padding_method, &
      !!     calc_input_gradients = .false., &
-     !!     activation_function = "relu", clip_dict = cv_clip))
+     !!     activation_function = "relu"))
      !!call network%add(maxpool3d_layer_type(&
      !!     pool_size = [2,2,1], stride = [2,2,1]))
      !!call network%add(full_layer_type( &
-     !!     num_outputs = 100, clip_dict = fc_clip, &
+     !!     num_outputs = 100, &
      !!     activation_function = "relu", &
      !!     kernel_initialiser = "he_uniform", &
      !!     bias_initialiser = "he_uniform" &
      !!     ))
      !!call network%add(full_layer_type( &
-     !!     num_outputs = 10, clip_dict = fc_clip,&
+     !!     num_outputs = 10,&
      !!     activation_function = "softmax", &
      !!     kernel_initialiser = "glorot_uniform", &
      !!     bias_initialiser = "glorot_uniform" &
