1e4b51b118ffb3995f1c7f91e95de2deaa327d71
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-09-14 07:47:49 +0100

    Move comments to TODO list

--- src/lib/mod_initialiser.f90
+++ src/lib/mod_initialiser.f90
@@ -17,16 +17,6 @@ module initialiser
 
   public :: initialiser_setup, get_default_initialiser
 
-!!!!! ALSO, HAVE THE CV, PL, FC, etc, LAYERS AS CLASSES
-!!!!! ... they may be able to be appended on to each other
-
-  
-  !! make an initialiser that takes in an assumed rank
-  !! it then does product(shape(weight)) OR size(weight)
-  !! could always use select rank(x) statement if needed
-  !! https://keras.io/api/layers/initializers/
-  
-  
 
 contains
 
@@ -51,7 +41,7 @@ contains
 
 
     !!--------------------------------------------------------------------------
-    !! 
+    !! set default initialiser based on activation
     !!--------------------------------------------------------------------------
     if(trim(activation).eq."selu")then
        name = "lecun_normal"
@@ -89,7 +79,6 @@ contains
        initialiser = he_normal
     case("lecun_uniform")
        initialiser = lecun_uniform
-       !initialiser%initialise() => lecun_uniform()
     case("lecun_normal")
        initialiser = lecun_normal
     case("zeros")
