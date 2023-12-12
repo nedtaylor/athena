fc50512d8055f0e4b5846f59959fe3ccb8d2e4e6
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-12-12 10:52:18 +0000

    Remove deprecated optimise procedure

--- src/lib/mod_optimiser.f90
+++ src/lib/mod_optimiser.f90
@@ -12,29 +12,14 @@ module optimiser
 !!!-----------------------------------------------------------------------------
 !!! learning parameter type
 !!!-----------------------------------------------------------------------------
-!!! MAKE THIS AN ABSTRACT TYPE WITH EXTENDED DERIVED TYPES FOR ADAM AND MOMENTUM
-!!! THEN MAKE REGULARISATION A SUBTYPE
-!!! NO, DON'T MAKE IT AN ABSTRACT TYPE, JUST MAKE IT A TYPE THAT HAS DERIVED TYPES FROM IT
   type, abstract :: base_optimiser_type !!base_optimiser_type
      character(:), allocatable :: method
-     !! reduce learning rate on plateau parameters
-     !integer :: wait = 0
-     !integer :: patience = 0
-     !real(real12) :: factor = 0._real12
-     !real(real12) :: min_learning_rate = 0._real12
-     !! step decay parameters
-     !real(real12) :: decay_rate = 0._real12
-     !real(real12) :: decay_steps = 0._real12
-     !! adam optimiser parameters
-     !real(real12) :: weight_decay  ! L2 regularisation on Adam (AdamW)
-
-     integer :: iter = 0
-     real(real12) :: learning_rate = 0.01_real12
-     !! momentum parameters
+     integer :: iter = 0  ! iteration number
+     real(real12) :: learning_rate = 0.01_real12  ! learning rate hyperparameter
      real(real12) :: momentum = 0._real12  ! fraction of momentum based learning
-     logical :: regularisation = .false.
-     class(base_regulariser_type), allocatable :: regulariser
-     type(clip_type) :: clip_dict
+     logical :: regularisation = .false.  ! apply regularisation
+     class(base_regulariser_type), allocatable :: regulariser  ! regularisation method
+     type(clip_type) :: clip_dict  ! clipping dictionary
    contains
      procedure, pass(this) :: init => init_base
      procedure(init_gradients), deferred, pass(this) :: init_gradients
@@ -76,6 +61,17 @@ module optimiser
      procedure, pass(this) :: init_gradients => init_gradients_adam
      procedure, pass(this) :: minimise => minimise_adam
   end type adam_optimiser_type
+
+     !! reduce learning rate on plateau parameters
+     !integer :: wait = 0
+     !integer :: patience = 0
+     !real(real12) :: factor = 0._real12
+     !real(real12) :: min_learning_rate = 0._real12
+     !! step decay parameters
+     !real(real12) :: decay_rate = 0._real12
+     !real(real12) :: decay_steps = 0._real12
+     !! adam optimiser parameters
+     !real(real12) :: weight_decay  ! L2 regularisation on Adam (AdamW)
 
 
   private
@@ -233,118 +229,6 @@ contains
     
   end subroutine minimise_adam
 !!!#############################################################################
-
-
-!!!##########################################################################!!!
-!!! * * * * * * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * !!!
-!!!##########################################################################!!!
-
-
-!!!#############################################################################
-!!! minimise the loss function by applying gradients to the parameters
-!!!#############################################################################
-  pure subroutine optimise(this, param, gradient)
-  implicit none
-  class(base_optimiser_type), intent(inout) :: this
-  real(real12), dimension(:), intent(inout) :: param
-  real(real12), dimension(:), intent(in) :: gradient
-
-  real(real12) :: t_learning_rate
-  real(real12), allocatable, dimension(:) :: lr_gradient
-
- !  lr_gradient = this%learning_rate * gradient
-
- !  !! adaptive learning method
- !  select case(this%method(1:1))
- !  case('m')!'momentum')
- !     !! momentum-based learning
- !     !! w = w - vel - lr * g
- !     this%velocity = lr_gradient + &
- !          this%momentum * this%velocity
- !  case('n')!('nesterov')
- !     !! nesterov momentum
- !     this%velocity = - this%momentum * this%velocity - &
- !          lr_gradient
- !  !case('a')!('adam')
- !  !   !! adam optimiser
- !  !   t_learning_rate = learning_rate
- !  !   call adam_optimiser(t_learning_rate, gradient, m, v)
- !  !   this%velocity = t_learning_rate
- !  case default
- !     this%velocity = lr_gradient
- !  end select
-
- !  !! regularisation
- !  if(this%regularise)then
- !     select case(this%regularisation)
- !     case('l1l2')
- !        !! L1L2 regularisation
- !        this%velocity = this%velocity + this%learning_rate * ( &
- !             this%l1 * sign(1._real12,param) + &
- !             2._real12 * this%l2 * param )
- !     case('l1')
- !        !! L1 regularisation
- !        this%velocity = this%velocity + this%learning_rate * &
- !             this%l1 * sign(1._real12,param)
- !     case('l2')
- !        !! L2 regularisation
- !        this%velocity = this%velocity + this%learning_rate * &
- !             2._real12 * this%l2 * param
- !     end select
- !  end if
-
- !  select case(this%method(1:1))
- !  case('n')!'nesterov')
- !     param = param + this%momentum * this%velocity - &
- !          lr_gradient
- !  case default
- !     param = param - this%velocity
- !  end select
-
-
-end subroutine optimise
-!!!#############################################################################
-
-
-!!!!############################################################################
-!!!! adaptive learning rate
-!!!! method: adam optimiser
-!!!!         ... Adaptive Moment Estimation
-!!!!############################################################################
-!!!! learning_rate = initial learning rate hyperparameter
-!!!! beta1 = exponential decay rate for first-moment estimates
-!!!! beta2 = exponential decay rate for second-moment estimates
-!!!! epsilon = small number for numerical stability
-!!!! t = current iteration
-!!!! m = first moment (m = mean of gradient)
-!!!! v = second moment (v = variance of the gradient)
-!  elemental subroutine adam(this, learning_rate, &
-!       gradient, m, v)
-!    implicit none
-!    class(optimiser_type), intent(in) :: this
-!    real(real12), intent(inout) :: learning_rate
-!    real(real12), intent(in) :: gradient
-!    real(real12), intent(inout) :: m, v
-!
-!    real :: m_norm, v_norm
-!
-!    !! update biased first moment estimate
-!    m = this%beta1 * m + (1._real12 - this%beta1) * gradient
-!
-!    !! update biased second moment estimate
-!    v = this%beta2 * v + (1._real12 - this%beta2) * gradient**2
-!
-!    !! normalised first moment estimate
-!    m_norm = m / (1._real12 - this%beta1**this%iter)
-!
-!    !! normalised second moment estimate
-!    v_norm = v / (1._real12 - this%beta2**this%iter)
-!    
-!    !! update learning rate
-!    learning_rate = learning_rate * m_norm / (sqrt(v_norm) + this%epsilon)
-!
-!  end subroutine adam
-!!!!############################################################################
 
 end module optimiser
 !!!#############################################################################
