e46eb6fdcd362eb234487df4dadd381a67e05a97
Ned Taylor <n.t.taylor@exeter.ac.uk>
2023-12-18 16:09:45 +0000

    Add avgpool reference

--- src/lib/mod_container_layer_sub.f90
+++ src/lib/mod_container_layer_sub.f90
@@ -4,7 +4,7 @@
 !!! Think Hepplestone, think HRG
 !!!#############################################################################
 submodule(container_layer) container_layer_submodule
-  use base_layer, only: learnable_layer_type
+  use base_layer, only: learnable_layer_type, flatten_layer_type
   use input1d_layer, only: input1d_layer_type
   use input3d_layer, only: input3d_layer_type
   use input4d_layer, only: input4d_layer_type
@@ -19,10 +19,6 @@ submodule(container_layer) container_layer_submodule
   use avgpool3d_layer, only: avgpool3d_layer_type
   use maxpool2d_layer, only: maxpool2d_layer_type
   use maxpool3d_layer, only: maxpool3d_layer_type
-  use flatten1d_layer, only: flatten1d_layer_type
-  use flatten2d_layer, only: flatten2d_layer_type
-  use flatten3d_layer, only: flatten3d_layer_type
-  use flatten4d_layer, only: flatten4d_layer_type
   use full_layer, only: full_layer_type
 
 contains
@@ -57,18 +53,16 @@ contains
     type is(dropblock3d_layer_type)
        call this%layer%forward(previous%output)
 
+    type is(avgpool2d_layer_type)
+       call this%layer%forward(previous%output)
+    type is(avgpool3d_layer_type)
+       call this%layer%forward(previous%output)
     type is(maxpool2d_layer_type)
        call this%layer%forward(previous%output)
     type is(maxpool3d_layer_type)
        call this%layer%forward(previous%output)
 
-    type is(flatten1d_layer_type)
-       call this%layer%forward(previous%output)
-    type is(flatten2d_layer_type)
-       call this%layer%forward(previous%output)
-    type is(flatten3d_layer_type)
-       call this%layer%forward(previous%output)
-    type is(flatten4d_layer_type)
+    class is(flatten_layer_type)
        call this%layer%forward(previous%output)
 
     type is(full_layer_type)
@@ -109,18 +103,16 @@ contains
     type is(dropblock3d_layer_type)
        call this%layer%backward(previous%output, gradient)
 
+    type is(avgpool2d_layer_type)
+       call this%layer%backward(previous%output, gradient)
+    type is(avgpool3d_layer_type)
+       call this%layer%backward(previous%output, gradient)
     type is(maxpool2d_layer_type)
        call this%layer%backward(previous%output, gradient)
     type is(maxpool3d_layer_type)
        call this%layer%backward(previous%output, gradient)
 
-    type is(flatten1d_layer_type)
-       call this%layer%backward(previous%output, gradient)
-    type is(flatten2d_layer_type)
-       call this%layer%backward(previous%output, gradient)
-    type is(flatten3d_layer_type)
-       call this%layer%backward(previous%output, gradient)
-    type is(flatten4d_layer_type)
+    class is(flatten_layer_type)
        call this%layer%backward(previous%output, gradient)
 
     type is(full_layer_type)
