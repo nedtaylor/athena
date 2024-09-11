0bb9c5f0f763aee903ac35db086c03ddcad8a55f
Ned Taylor <nedttaylor@gmail.com>
2024-09-11 12:02:51 +0100

    Fix code coverage from brew

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -140,7 +140,7 @@ include(CMakeParseArguments)
 option(CODE_COVERAGE_VERBOSE "Verbose information" FALSE)
 
 # Check prereqs
-find_program( GCOV_PATH gcov )
+find_program( GCOV_PATH gcov-14 gcov )
 find_program( LCOV_PATH  NAMES lcov lcov.bat lcov.exe lcov.perl)
 find_program( FASTCOV_PATH NAMES fastcov fastcov.py )
 find_program( GENHTML_PATH NAMES genhtml genhtml.perl genhtml.bat )
