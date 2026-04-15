72f03d2d56ec438753d19c97e2f0565d2bc2fff3
Ned Taylor <71959356+nedtaylor@users.noreply.github.com>
2026-04-15 11:10:33 +0100

    Update package file

--- .spack/packages/athena-fortran/package.py
+++ .spack/packages/athena-fortran/package.py
@@ -1,24 +1,8 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
 #
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install athena_fortran
-#
-# You can edit this file again by typing:
-#
-#     spack edit athena_fortran
-#
-# See the Spack documentation for more information on packaging.
 # NOTE: the package name has been set to "athena_fortran" to avoid conflicts with the existing "athena" package. Please rename as appropriate.
-# ----------------------------------------------------------------------------
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
 
@@ -26,7 +10,7 @@ from spack.package import *
 
 
 class AthenaFortran(CMakePackage):
-    """A Fortran library for building, training and testing neural networks."""
+    """ATHENA is a Fortran library for building, training and testing neural networks."""
 
     homepage = "https://athena-fortran.readthedocs.io/en/latest/"
     url = "https://github.com/nedtaylor/athena/archive/refs/tags/v2.1.0.tar.gz"
@@ -36,14 +20,15 @@ class AthenaFortran(CMakePackage):
 
     license("MIT", checked_by="nedtaylor")
 
+    # --- Versions ---
     version("development", branch="development")
     version("main", branch="main")
     version("2.1.0", sha256="f468d077378e61ec7f4b74ee75cabf6c244d34afc4bd6930c2990727056cc7c1")
     version("2.0.0", sha256="db15a25b73d4f94dea3fb119df2be1974cdfe52209b0437eb9dcdf8fd34fa834")
+    version("1.3.3", sha256="c73a908bc2c5b006f8acb316976416cd1661b458a0c95f5819f24bf7bf281f24")
 
-
+    # --- Variants ---
     variant("mpi", default=False, description="Enable MPI support")
-
 
     # --- Dependencies ---
     depends_on("cmake@3.18:", type="build")
@@ -55,6 +40,10 @@ class AthenaFortran(CMakePackage):
     depends_on("lapack")
 
     # --- Compiler constraints ---
+    conflicts("%gcc@:14.2", when="@main", msg="Requires GCC 14.3 or later for Fortran support")
+    conflicts("%gcc@:14.2", when="@development", msg="Requires GCC 14.3 or later for Fortran support")
+    conflicts("%gcc@:14.2", when="@2.0.0:", msg="Requires GCC 14.3 or later for Fortran support")
+    conflicts("%gcc@:11.4", when="@1.3.3", msg="Requires GCC 12.1 or later for Fortran support")
     conflicts("%apple-clang", msg="Requires a Fortran-capable compiler (e.g. GCC)")
 
     # --- Version URL handling ---
